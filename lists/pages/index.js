fb1a5387c46f3b1f49df44cbd29af1a6f3998736
tricia gray <tgray@tricias-MacBook-Pro.local>
2023-03-31 08:46:57 -0400

    rough draft p1

---
+++ lists/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>List</title>
+      </Head>
+      <h2>Sight and Sound Critics' Poll: Greatest Films of All Time</h2>
+    </div>
+  )
+}
+
+export default Home;
