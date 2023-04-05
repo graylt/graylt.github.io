8f33fdbc829db56958bf713b29e38442b670e962
tricia gray <tgray@tricias-MacBook-Pro.local>
2023-04-05 18:16:18 -0400

    migrated cra to vite

---
+++ weather/src/App.jsx
@@ -0,0 +1,159 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+const Weather = ({ country }) => {
+	const [weatherData, setWeatherData] = useState({
+		weather: [{ icon: "01d" }],
+		main: { temp: 0 },
+		wind: { speed: 0 },
+	})
+	const key = import.meta.env.REACT_APP_API_KEY
+	const latitude = country.capitalInfo.latlng[0]
+	const longitude = country.capitalInfo.latlng[1]
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`
+	const iconCode = weatherData.weather[0].icon
+	const imageLink = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
+	const temperature = weatherData.main.temp
+	const wind = weatherData.wind.speed
+
+	function fetchHook(link) {
+		const eventHandler = response => {
+			setWeatherData(response.data)
+		}
+		axios.get(link).then(eventHandler)
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
+		console.log(link)
+	} 
+
+	useEffect(() => {
+		fetchHook(url)
+	}, [])
+
+	return (
+		<>
+			<h3>Weather in {country.capital}</h3>
+			<div>temperature {temperature.toFixed(2)} Celcius</div>
+			<img src={imageLink} alt="weatherIcon" />
+			<div>wind {wind} m/s</div>
+		</>
+	)
+}
+
+const Countries = ({ countriesToShow, newSearchCountry, handleClick }) => {
+	if (newSearchCountry === "") {
+		return <div>Search a Country</div>
+	} else {
+		if (countriesToShow.length > 10) {
+			return <div>Too many matches, specify another filter</div>
+		} else {
+			if (countriesToShow.length > 1) {
+				return (
+					<>
+						{countriesToShow.map((country, i) => (
+							<div key={i}>
+								{country.name.common}{" "}
+								<button
+									onClick={() =>
+										handleClick(country.name.common)
+									}
+								>
+									show
+								</button>
+							</div>
+						))}
+					</>
+				)
+			} else {
+				if (countriesToShow.length === 1) {
+					let latitude = countriesToShow[0].capitalInfo.latlng[0]
+					let longitude = countriesToShow[0].capitalInfo.latlng[1]
+					let key = "123"
+					let units = "metric"
+					return (
+						<div>
+							<h1>{countriesToShow[0].name.common}</h1>
+							<div>capital {countriesToShow[0].capital}</div>
+							<div>area {countriesToShow[0].area}</div>
+							<h3>languages:</h3>
+							<ul>
+								{Object.values(
+									countriesToShow[0].languages
+								).map(value => (
+									<li key={value}>{value}</li>
+								))}
+							</ul>
+							<img
+								alt={countriesToShow[0].flag}
+								src={countriesToShow[0].flags.png}
+							/>
+							<Weather country={countriesToShow[0]} />
+						</div>
+					)
+				} else {
+					return <div>0 matches, try another search</div>
+				}
+			}
+		}
+	}
+}
+function App() {
+	const [newSearchCountry, setNewSearchCountry] = useState("")
+	const [countries, setCountries] = useState([])
+
+	function handleClick(countryName) {
+		setNewSearchCountry(countryName)
+	}
+
+	const fetchHook = () => {
+		const eventHandler = response => {
+			setCountries(response.data)
+		}
+		axios.get("https://restcountries.com/v3.1/all").then(eventHandler)
+	}
+
+	const handleSearchCountry = event => {
+		setNewSearchCountry(event.target.value)
+	}
+
+	useEffect(fetchHook, [])
+
+	const countriesToShow = countries.filter(country =>
+		country.name.common
+			.toLowerCase()
+			.includes(newSearchCountry.toLocaleLowerCase())
+	)
+
+	return (
+		<div>
+			find countries{" "}
+			<input value={newSearchCountry} onChange={handleSearchCountry} />
+			<Countries
+				countriesToShow={countriesToShow}
+				newSearchCountry={newSearchCountry}
+				handleClick={handleClick}
+			/>
+		</div>
+	)
+}
+
+export default App
---
+++ weather/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import envCompatible from 'vite-plugin-env-compatible';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  envPrefix: 'REACT_APP_',
+  // define: {
+  //   'process.env': {}
+  // },
+
+  plugins: [react(),
+    envCompatible
+  ],
+})
